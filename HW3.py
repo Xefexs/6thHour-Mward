822b613dc8a1774a1417a21fc9f0dd22a67283c8
Matthew Ward <Mw126132@student.wpsok.org>
2026-08-27 13:36:38 -0500

    HW2.py

---
+++ HW3.py
@@ -0,0 +1,28 @@
+#Name:Matthew ward
+#Class: 6th Hour
+#Assignment: HW3
+
+
+#1. Print "Hello World!"
+print("hello world")
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+stringvar1 = ("hello World")
+integervar2 = 4
+booleanvar3 = True
+#3. Print all three variables on the same print function (at the same time).
+print(stringvar1,integervar2,booleanvar3)
+#4. Create a variable that asks the user to input an integer.
+a=int(input("pick an integer"))
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+int_sum=(integervar2+a)
+print(int_sum)
+#6. Take the result from #5 and divide it by 2. Print the result.
+print(int_sum/2)
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+booleanvar3 = False
+#8. Print the value of the boolean variable.
+print(booleanvar3)
+#9. Create a variable with a number that contains decimals.
+integervar4 = 2.5
+#10. Round the number from #9 up or down using the round function.
+print(round(integervar4))
